9a72ca63d26b12ef5e1f8ecaf322b0e09e3af3c6
JihadMuna <jojo.muna.2019@gmail.com>
2024-01-22 20:47:42 +0200

    edditing babysitters cards

--- client/src/components/BabySittersPage.jsx
+++ client/src/components/BabySittersPage.jsx
@@ -1,21 +1,17 @@
 import React, { useEffect, useState } from "react";
 import useBabysittersList from "../hooks/useBabysittersList";
-import SignupBabySitters from "./SignupBabySitters";
+import Spinner from "react-bootstrap/Spinner";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const BabySittersPage = () => {
-  const { babysittersList, loading, error, createBabysitter, loginBabysitter } =
-    useBabysittersList();
-  const [allBabysitters, setAllBabysitters] = useState([]);
+  const { babysittersList, loading, error } = useBabysittersList();
 
-  useEffect(() => {
-    // Fetch babysitters on component mount
-    // Update the state with the fetched babysittersList
-    setAllBabysitters(babysittersList);
-  }, [babysittersList]);
-
-  // Handle loading and error states
   if (loading) {
-    return <div>...loading</div>;
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Spinner animation="border" variant="primary" />
+      </div>
+    );
   }
 
   if (error) {
@@ -23,35 +19,43 @@ const BabySittersPage = () => {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10 p-6 bg-slate-300">
       {babysittersList.map((babysitter) => (
-        <div key={babysitter._id} className="bg-white p-4 shadow-md rounded-md">
-          <img
-            src={babysitter.image}
-            alt={`Image of ${babysitter.username}`}
-            className="w-full h-32 object-cover mb-4 rounded-md"
-          />
-          <h2 className="text-lg font-semibold mb-2">{babysitter.username}</h2>
-          <p className="text-gray-600">{babysitter.description}</p>
+        <div
+          key={babysitter._id}
+          className="bg-white p-4 shadow-md rounded-md flex flex-col justify-between"
+        >
+          <div>
+            <img
+              src={babysitter.image}
+              alt={`Image of ${babysitter.username}`}
+              className="w-full h-32 object-cover mb-2 rounded-md"
+            />
+            <h2 className="text-lg font-semibold mb-1">
+              {babysitter.username}
+            </h2>
+            <p className="text-gray-600 mb-2">{babysitter.description}</p>
 
-          <div className="mt-4">
-            <p className="text-sm text-gray-500">Age: {babysitter.age}</p>
-            <p className="text-sm text-gray-500">
-              Address: {babysitter.address}
-            </p>
-            <p className="text-sm text-gray-500">
-              Work Area: {babysitter.workLocationArea}
-            </p>
-            <p className="text-sm text-gray-500">
-              Experience: {babysitter.experience}
-            </p>
-            <p className="text-sm text-gray-500">
-              Availability: {babysitter.availability}
-            </p>
+            <div className="grid grid-cols-2 gap-2">
+              <div className="text-sm font-bold text-gray-700">
+                <p>Age:</p>
+                <p>Address:</p>
+                <p>Work Area:</p>
+                <p>Experience:</p>
+                <p>Availability:</p>
+              </div>
+              <div className="text-sm text-gray-500">
+                <p>{babysitter.age}</p>
+                <p>{babysitter.address}</p>
+                <p>{babysitter.workLocationArea}</p>
+                <p>{babysitter.experience}</p>
+                <p>{babysitter.availability}</p>
+              </div>
+            </div>
           </div>
 
-          <button className="mt-4 bg-pink-500 text-white py-2 px-4 rounded hover:bg-pink-600">
-            Contact
+          <button className="mt-2 bg-pink-500 text-white py-2 px-4 rounded hover:bg-pink-600 flex items-center justify-center">
+            Contact <FaPhoneAlt className="ml-2" />
           </button>
         </div>
       ))}
